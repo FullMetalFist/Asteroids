b6687f1e3f147cfd5f53b048a8d64a938d048bca
Michael Vilabrera <m.vilabrera@gmail.com>
2018-02-15 14:40:15 -0500

    initial commit

---
+++ Asteroids/GameScene.swift
@@ -0,0 +1,30 @@
+//
+//  GameScene.swift
+//  Asteroids
+//
+//  Created by Michael Vilabrera on 2/15/18.
+//  Copyright © 2018 Michael Vilabrera. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+@objcMembers
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        // called when game scene is ready to run
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // called when user touchers screen
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // called when user stops touching screen
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // called before each frame is rendered
+    }
+}
