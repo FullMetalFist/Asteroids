f2fb5c4588637870b7a7899bd3641c99a777c951
Michael Vilabrera <m.vilabrera@gmail.com>
2018-02-25 13:25:09 -0500

    making player move track, implement touches moved to move player character

--- Asteroids/GameScene.swift
+++ Asteroids/GameScene.swift
@@ -13,6 +13,7 @@ import GameplayKit
 class GameScene: SKScene {
     
     let player = SKSpriteNode(imageNamed: "player-rocket.png")
+    var touchingPlayer = false
     
     override func didMove(to view: SKView) {
         // called when game scene is ready to run
@@ -33,10 +34,26 @@ class GameScene: SKScene {
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         // called when user touchers screen
+        guard let touch = touches.first else {
+            return
+        }
+        let location = touch.location(in: self)
+        let tappedNodes = nodes(at: location)
+        if tappedNodes.contains(player) {
+            touchingPlayer = true
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard touchingPlayer else { return }
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        player.position = location
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         // called when user stops touching screen
+        touchingPlayer = false
     }
     
     override func update(_ currentTime: TimeInterval) {
