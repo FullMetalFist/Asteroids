7bd6056e0ecfd839173f1ed5b3fccaa52a0b9fb9
Michael Vilabrera <m.vilabrera@gmail.com>
2018-02-26 10:46:01 -0500

    WIP add timer, player & enemy physics bodies (with gravity)

--- Asteroids/GameScene.swift
+++ Asteroids/GameScene.swift
@@ -10,10 +10,12 @@ import SpriteKit
 import GameplayKit
 
 @objcMembers
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     let player = SKSpriteNode(imageNamed: "player-rocket.png")
     var touchingPlayer = false
+    
+    var gameTimer: Timer?
     
     override func didMove(to view: SKView) {
         // called when game scene is ready to run
@@ -30,6 +32,12 @@ class GameScene: SKScene {
         player.position.x = -300
         player.zPosition = 1
         addChild(player)
+        
+        player.physicsBody = SKPhysicsBody(texture: player.texture!, size: player.size)
+        player.physicsBody?.categoryBitMask = 1
+        
+        gameTimer = Timer.scheduledTimer(timeInterval: 0.35, target: self, selector: #selector(createEnemy), userInfo: nil, repeats: true)
+        physicsWorld.contactDelegate = self
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -58,5 +66,36 @@ class GameScene: SKScene {
     
     override func update(_ currentTime: TimeInterval) {
         // called before each frame is rendered
+    }
+    
+    func createEnemy() {
+        let randomDistribution = GKRandomDistribution(lowestValue: -350, highestValue: 350)
+        
+        let sprite = SKSpriteNode(imageNamed: "asteroid")
+        sprite.position = CGPoint(x: 200, y: randomDistribution.nextInt())
+        sprite.name = "enemy"
+        sprite.zPosition = 1
+        addChild(sprite)
+        
+        sprite.physicsBody = SKPhysicsBody(texture: sprite.texture!, size: sprite.size)
+        sprite.physicsBody?.velocity = CGVector(dx: -500, dy: 0)
+        sprite.physicsBody?.linearDamping = 0
+        sprite.physicsBody?.contactTestBitMask = 1
+        sprite.physicsBody?.categoryBitMask = 0
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        guard let nodeA = contact.bodyA.node else { return }
+        guard let nodeB = contact.bodyB.node else { return }
+        
+        if nodeA == player {
+            playerHit(nodeB)
+        } else {
+            playerHit(nodeA)
+        }
+    }
+    
+    func playerHit(_ node: SKNode) {
+        player.removeFromParent()
     }
 }
